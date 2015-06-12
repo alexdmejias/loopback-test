7e7011fe467978514ead0854c5336d0eb022aa50
Alex Mejias <alexdmejias@gmail.com>
2015-06-12 16:56:45 -0400

    more stuff

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -1,20 +1,6 @@
 var path = require('path');
 var app = require(path.resolve(__dirname, '../server'));
 
-var dataSource = app.dataSources.postgres;
-
-dataSource.discoverSchema('car', {schema: 'public'},
-  function(err, schema) {
-	  if (err) throw err;
-
-	  if (schema) {
-			// console.log(JSON.stringify(schema, null, '  '));
-			console.log('found schema, updating');
-			dataSource.autoupdate('car');
-		} else {
-			console.log('schema not found');
-			dataSource.automigrate('car');
-		}
-
-	  dataSource.disconnect();
+app.dataSources.postgres.automigrate(['car','Make'], function(err) {
+  console.log(err);
 });
