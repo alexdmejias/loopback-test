13ff00c03a45d8db7404bc6ce90857c8e90d1e3c
Alex Mejias <alexdmejias@gmail.com>
2015-06-16 10:55:52 -0400

    better migrations and seeding

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -1,6 +1,20 @@
 var path = require('path');
 var app = require(path.resolve(__dirname, '../server'));
+var seeds = require('./db-seed.json');
 
-app.dataSources.postgres.autoupdate(['car','Make'], function(err) {
-  console.log(err);
-});
+if (process.env.NODE_ENV === "development") {
+  app.dataSources.postgres.automigrate(['Car','Make', 'Owner'], function(err) {
+    console.log('deleting previous tables and seeding database');
+    seeds.models.forEach(function(seed) {
+      app.models[seed.name].create(seed.data);
+    });
+  });
+} else {
+  app.dataSources.postgres.autoupdate(['Car','Make'], function(err) {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      console.log('updated models, no errors');
+    }
+  });
+}
